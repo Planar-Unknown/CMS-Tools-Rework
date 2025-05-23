460f1ddb890d657ff9a671544adf239e09f65f6e
Dreu <mills1234a@gmail.com>
2025-05-23 14:23:03 -0700

    Fixed Null Reference bug

--- src/main/java/com/dreu/planartools/mixin/BlockStateBaseMixin.java
+++ src/main/java/com/dreu/planartools/mixin/BlockStateBaseMixin.java
@@ -2,13 +2,17 @@ package com.dreu.planartools.mixin;
 
 import com.dreu.planartools.config.BlocksConfig;
 import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import static com.dreu.planartools.config.BlocksConfig.BLOCKS;
 
@@ -17,13 +21,19 @@ import static com.dreu.planartools.config.BlocksConfig.BLOCKS;
 public class BlockStateBaseMixin {
     @Shadow private float destroySpeed;
 
-    @Overwrite
-    public float getDestroySpeed(BlockGetter level, BlockPos blockPos) {
-        @SuppressWarnings("DataFlowIssue") BlocksConfig.Properties blockProperties = BLOCKS.get(ForgeRegistries.BLOCKS.getKey(this.getBlock()).toString());
-        return blockProperties != null ? blockProperties.hardness().orElse(this.destroySpeed) : this.destroySpeed;
+    @Inject(method = "getDestroySpeed", at = @At("HEAD"), cancellable = true)
+    public void onGetDestroySpeed(BlockGetter level, BlockPos blockPos, CallbackInfoReturnable<Float> cir) {
+        ResourceLocation resourceLocation = ForgeRegistries.BLOCKS.getKey(getBlock());
+        if (resourceLocation != null) {
+            @SuppressWarnings("DataFlowIssue")
+            BlocksConfig.Properties blockProperties = BLOCKS.get(resourceLocation.toString());
+            cir.setReturnValue(blockProperties != null ? blockProperties.hardness().orElse(this.destroySpeed) : this.destroySpeed);
+        }
     }
 
-
+    private BlockState self() {
+        return (BlockState) (Object) this;
+    }
 
     @SuppressWarnings("SameReturnValue")
     @Shadow public Block getBlock() {
--- src/main/java/com/dreu/planartools/mixin/DiggerItemMixin.java
+++ src/main/java/com/dreu/planartools/mixin/DiggerItemMixin.java
@@ -2,35 +2,22 @@ package com.dreu.planartools.mixin;
 
 import com.dreu.planartools.config.BlocksConfig;
 import com.dreu.planartools.config.ToolsConfig;
-import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.DiggerItem;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Tier;
-import net.minecraft.world.item.TieredItem;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import static com.dreu.planartools.config.BlocksConfig.BLOCKS;
 import static com.dreu.planartools.config.ToolsConfig.TOOLS;
 
 @Mixin(DiggerItem.class) @SuppressWarnings("unused")
-public class DiggerItemMixin extends TieredItem {
-
-    private final TagKey<Block> blocks;
-    private final float speed;
-
-    //dummy constructor to trick the pre-compiler
-    public DiggerItemMixin(Tier tier, TagKey<Block> blocks, Properties properties) {
-        super(tier, properties);
-        this.blocks = blocks;
-        this.speed = tier.getSpeed();
-    }
-
-    @Overwrite @SuppressWarnings("DataFlowIssue")
-    public float getDestroySpeed(ItemStack itemInHand, BlockState blockState) {
+public class DiggerItemMixin {
+    @Inject(method = "getDestroySpeed", at = @At("HEAD"), cancellable = true) @SuppressWarnings("DataFlowIssue")
+    public void onGetDestroySpeed(ItemStack itemInHand, BlockState blockState, CallbackInfoReturnable<Float> cir) {
         ToolsConfig.Properties toolProperties = TOOLS.get(ForgeRegistries.ITEMS.getKey(itemInHand.getItem()).toString());
         BlocksConfig.Properties blockProperties = BLOCKS.get(ForgeRegistries.BLOCKS.getKey(blockState.getBlock()).toString());
         if (toolProperties != null && blockProperties != null) {
@@ -49,8 +36,7 @@ public class DiggerItemMixin extends TieredItem {
                         canMine = true;
                 }
             }
-            return canMine ? applyMiningSpeed ? toolProperties.miningSpeed() : 1.0f : 0.0f;
+            cir.setReturnValue(canMine ? applyMiningSpeed ? toolProperties.miningSpeed() : 1.0f : 0.0f);
         }
-        return blockState.is(blocks) ? speed : 1.0F;
     }
 }
