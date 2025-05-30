e52e9ac185161ecef176ea8b88d894ceec855992
Dreu <mills1234a@gmail.com>
2025-05-30 00:39:55 -0700

    Minor refactor

--- src/main/java/com/dreu/planartools/mixin/BlockStateBaseMixin.java
+++ src/main/java/com/dreu/planartools/mixin/BlockStateBaseMixin.java
@@ -2,7 +2,6 @@ package com.dreu.planartools.mixin;
 
 import com.dreu.planartools.config.BlocksConfig;
 import net.minecraft.core.BlockPos;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -16,27 +15,25 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import static com.dreu.planartools.config.BlocksConfig.BLOCKS;
 
-@SuppressWarnings("unused")
+@SuppressWarnings({"unused", "DataFlowIssue"})
 @Mixin(BlockBehaviour.BlockStateBase.class)
 public class BlockStateBaseMixin {
-    @Shadow private float destroySpeed;
+  @Shadow
+  private float destroySpeed;
 
-    @Inject(method = "getDestroySpeed", at = @At("HEAD"), cancellable = true)
-    public void onGetDestroySpeed(BlockGetter level, BlockPos blockPos, CallbackInfoReturnable<Float> cir) {
-        ResourceLocation resourceLocation = ForgeRegistries.BLOCKS.getKey(getBlock());
-        if (resourceLocation != null) {
-            @SuppressWarnings("DataFlowIssue")
-            BlocksConfig.Properties blockProperties = BLOCKS.get(resourceLocation.toString());
-            cir.setReturnValue(blockProperties != null ? blockProperties.hardness().orElse(this.destroySpeed) : this.destroySpeed);
-        }
-    }
+  @Inject(method = "getDestroySpeed", at = @At("HEAD"), cancellable = true)
+  public void onGetDestroySpeed(BlockGetter level, BlockPos blockPos, CallbackInfoReturnable<Float> cir) {
+    BlocksConfig.Properties blockProperties = BLOCKS.get(ForgeRegistries.BLOCKS.getKey(getBlock()).toString());
+    cir.setReturnValue(blockProperties != null ? blockProperties.hardness().orElse(this.destroySpeed) : this.destroySpeed);
+  }
 
-    private BlockState self() {
-        return (BlockState) (Object) this;
-    }
+  private BlockState self() {
+    return (BlockState) (Object) this;
+  }
 
-    @SuppressWarnings("SameReturnValue")
-    @Shadow public Block getBlock() {
-        return null;
-    }
+  @SuppressWarnings("SameReturnValue")
+  @Shadow
+  public Block getBlock() {
+    return null;
+  }
 }
