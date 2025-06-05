1ef169d3e49dd288092a7b9326ea5086220f0b0b
Dreu <mills1234a@gmail.com>
2025-06-05 00:02:00 -0700

    Improved Compatibility with other mods

--- src/main/java/com/dreu/planartools/mixin/ItemStackMixin.java
+++ src/main/java/com/dreu/planartools/mixin/ItemStackMixin.java
@@ -2,38 +2,76 @@ package com.dreu.planartools.mixin;
 
 import com.dreu.planartools.config.BlocksConfig;
 import com.dreu.planartools.config.ToolsConfig;
+import com.dreu.planartools.util.OpposingSets;
 import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.ShearsItem;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.common.TierSortingRegistry;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 import java.util.Map;
+import java.util.TreeMap;
 
 import static com.dreu.planartools.PlanarTools.TAG_KEYS_BY_TOOL_TYPE;
 import static com.dreu.planartools.config.BlocksConfig.getBlockProperties;
+import static com.dreu.planartools.config.EnchantsConfig.ENCHANTS_BY_ITEM_ID;
+import static com.dreu.planartools.config.EnchantsConfig.ENCHANTS_BY_TOOL_TYPE;
+import static com.dreu.planartools.config.ToolsConfig.TOOLS;
 import static com.dreu.planartools.config.ToolsConfig.getToolProperties;
 import static com.dreu.planartools.util.Helpers.getTierIfPresent;
 
-@SuppressWarnings("unused")
-@Mixin(ShearsItem.class)
-public class ShearsItemMixin {
-  @SuppressWarnings("DataFlowIssue")
-  private Item self() {
-    return (Item) (Object) this;
+@SuppressWarnings({"unused", "DataFlowIssue"})
+@Mixin(ItemStack.class)
+public class ItemStackMixin {
+
+  @Redirect(
+      method = "isEnchantable",
+      at = @At(
+          value = "INVOKE",
+          target = "Lnet/minecraft/world/item/Item;isEnchantable(Lnet/minecraft/world/item/ItemStack;)Z"
+      )
+  )
+  private boolean redirectItemIsEnchantable(Item item, ItemStack itemStack) {
+    System.out.println("We are at: ItemStickyMixin yay!");
+    String itemId = ForgeRegistries.ITEMS.getKey(itemStack.getItem()).toString();
+    OpposingSets<String> byItem = ENCHANTS_BY_ITEM_ID.getOrDefault(itemId, new OpposingSets<>());
+    if (!byItem.positive().isEmpty()) return true;
+
+    ToolsConfig.Properties toolProperties = TOOLS.get(itemId);
+    if (toolProperties != null) {
+      for (Byte toolType : toolProperties.powers().keySet()) {
+        TreeMap<Integer, OpposingSets<String>> treeMap = ENCHANTS_BY_TOOL_TYPE.get(toolType);
+        if (treeMap != null && !treeMap.isEmpty()) {
+          for (OpposingSets<String> enchantments : treeMap.descendingMap().values()) {
+            if (!enchantments.positive().isEmpty()) {
+              for (String enchant : enchantments.positive()) {
+                if (!byItem.negative().contains(enchant)) return true;
+              }
+            }
+          }
+        }
+      }
+    }
+
+    return item.isEnchantable(itemStack);
   }
 
-  @Inject(method = "getDestroySpeed", at = @At("HEAD"), cancellable = true)
-  private void onGetDestroySpeed(ItemStack itemInHand, BlockState blockState, CallbackInfoReturnable<Float> cir) {
+  @Redirect(
+      method = "getDestroySpeed",
+      at = @At(
+          value = "INVOKE",
+          target = "Lnet/minecraft/world/item/Item;getDestroySpeed(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/level/block/state/BlockState;)F"
+      )
+  )
+  private float redirectGetDestroySpeed(Item item, ItemStack itemStack, BlockState blockState) {
     BlocksConfig.Properties blockProperties = getBlockProperties(blockState.getBlock());
-    ToolsConfig.Properties toolProperties = getToolProperties(self());
+    ToolsConfig.Properties toolProperties = getToolProperties(item);
     if (blockProperties != null) {
       boolean applyMiningSpeed = false;
       if (toolProperties != null) {
@@ -57,30 +95,38 @@ public class ShearsItemMixin {
         if (canMine) {
           if (applyMiningSpeed) {
             if (toolProperties.miningSpeed().isPresent())
-              cir.setReturnValue(Float.valueOf(toolProperties.miningSpeed().get()));
+              return Float.valueOf(toolProperties.miningSpeed().get());
           } else
-            cir.setReturnValue(1f);
+            return 1f;
         } else
-          cir.setReturnValue(0f);
+          return 0f;
       } else {
-        cir.setReturnValue(blockProperties.defaultResistance() == 0 ? 1f : 0f);
+        return blockProperties.defaultResistance() == 0 ? 1f : 0f;
       }
     } else if (toolProperties != null) {
-      cir.setReturnValue(isCorrectToolForDrops(blockState) ? toolProperties.miningSpeed().orElse(1) : 1.0f);
+      return isCorrectToolForDrops(blockState) ? toolProperties.miningSpeed().orElse(1) : 1.0f;
     }
+    return item.getDestroySpeed(itemStack, blockState);
   }
 
-  @Inject(method = "isCorrectToolForDrops", at = @At("HEAD"), cancellable = true)
-  private void onIsCorrectToolForDrops(BlockState blockState, CallbackInfoReturnable<Boolean> cir) {
+  @Redirect(
+      method = "isCorrectToolForDrops",
+      at = @At(
+          value = "INVOKE",
+          target = "Lnet/minecraft/world/item/Item;isCorrectToolForDrops(Lnet/minecraft/world/item/ItemStack;Lnet/minecraft/world/level/block/state/BlockState;)Z",
+          remap = false
+      )
+  )
+  private boolean redirectIsCorrectToolForDrops(Item item, ItemStack itemStack, BlockState blockState) {
     BlocksConfig.Properties blockProperties = getBlockProperties(blockState.getBlock());
-    ToolsConfig.Properties toolProperties = getToolProperties(self());
+    ToolsConfig.Properties toolProperties = getToolProperties(item);
     if (toolProperties != null) {
       if (blockProperties != null) {
         for (Map.Entry<Byte, Integer> powerData : toolProperties.powers().entrySet()) {
           BlocksConfig.ResistanceData resistanceData = blockProperties.data().get(powerData.getKey());
           if (resistanceData != null) {
             if (resistanceData.resistance() >= 0 && powerData.getValue() >= resistanceData.resistance()) {
-              cir.setReturnValue(true);
+              return true;
             }
           }
         }
@@ -90,12 +136,13 @@ public class ShearsItemMixin {
           if (blockState.is(tag)) {
             var tier = getTierIfPresent(powerData.getKey(), toolProperties);
             if (tier != null && TierSortingRegistry.isCorrectTierForDrops(tier, blockState)) {
-              cir.setReturnValue(true);
+              return true;
             }
           }
         }
       }
     }
+    return item.isCorrectToolForDrops(itemStack, blockState);
   }
 
   @SuppressWarnings("SameReturnValue")
