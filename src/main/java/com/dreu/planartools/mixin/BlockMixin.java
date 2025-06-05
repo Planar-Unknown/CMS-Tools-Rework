5c9bc332337db356a189eb35749772335d05a715
Dreu <mills1234a@gmail.com>
2025-06-04 22:03:43 -0700

    Refactor

--- src/main/java/com/dreu/planartools/mixin/BlockMixin.java
+++ src/main/java/com/dreu/planartools/mixin/BlockMixin.java
@@ -1,26 +1,21 @@
 package com.dreu.planartools.mixin;
 
 import com.dreu.planartools.config.BlocksConfig;
-import com.dreu.planartools.util.CachedSupplier;
 import net.minecraft.world.level.block.Block;
-import net.minecraftforge.registries.ForgeRegistries;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import static com.dreu.planartools.config.BlocksConfig.BLOCKS;
+import static com.dreu.planartools.config.BlocksConfig.getBlockProperties;
 
 @Mixin(Block.class)
 @SuppressWarnings("unused")
 public class BlockMixin {
-    @SuppressWarnings("DataFlowIssue")
-    private final CachedSupplier<BlocksConfig.Properties> blockProperties = CachedSupplier.of(() -> BLOCKS.get(ForgeRegistries.BLOCKS.getKey(this.asBlock()).toString()));
-
     @Inject(method = "getExplosionResistance", at = @At("HEAD"), cancellable = true)
     private void onGetExplosionResistance(CallbackInfoReturnable<Float> cir) {
-        BlocksConfig.Properties prop = blockProperties.get();
+        BlocksConfig.Properties prop = getBlockProperties(asBlock());
         if (prop != null) {
             float expRes = prop.explosionResistance().orElse(this.asBlock().explosionResistance);
             cir.setReturnValue(expRes == -1 ? Float.POSITIVE_INFINITY : expRes);
